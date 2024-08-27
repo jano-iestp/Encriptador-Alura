0fdf18f869149e5ee9b0e3707ca3c66dd32ad841
jano <137105532+jano-iestp@users.noreply.github.com>
2024-08-27 09:21:25 -0500

    Update encriptador mayusculas.js

--- encriptador.js
+++ encriptador.js
@@ -58,7 +58,7 @@ textArea.addEventListener("input", (e) => {
 //Funcion del boton encriptar
 botonEncriptar.addEventListener("click", (e) => {
   e.preventDefault();
-  let mensaje = textArea.value.toLowerCase();
+  let mensaje = textArea.value;
   
   // Validar antes de encriptar
   if (!validarMensaje(mensaje)) {
@@ -74,7 +74,7 @@ botonEncriptar.addEventListener("click", (e) => {
 
 botonDesencriptar[1].addEventListener("click", (e) => {
   e.preventDefault();
-  let mensaje = textArea.value.toLowerCase();
+  let mensaje = textArea.value;
   
   // Validar antes de desencriptar
   if (!validarMensaje(mensaje)) {
